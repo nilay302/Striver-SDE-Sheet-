891d732f6cc835b4c5d003c6f1f0ca5b15256f24
nilay302 <nilayshirke52@gmail.com>
2022-07-05 18:50:26 +0530

    rotate and pow

---
+++ Day 2/RotateMatrix.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for(int i = 0;i<n;i++){
+            for(int j = i;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);   //Transpose the matrix
+            }
+        }
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n/2;j++){
+                swap(matrix[i][j],matrix[i][n-j-1]);  //Reverse each row
+            }
+        }
+    }
+};
