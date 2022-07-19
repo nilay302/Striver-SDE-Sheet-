5c59ea0987d1c27fb75b27a5f97ee13282608930
nilay302 <nilayshirke52@gmail.com>
2022-07-20 02:31:24 +0530

    cycle in a ll

---
+++ Day 6 Linked List/LinkedListCycle.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+  class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode * slow = head;
+        ListNode * fast = head;
+        do{
+            if(slow==NULL || fast == NULL)return NULL;
+            slow = slow->next;
+            fast = fast->next;
+            if(fast==NULL)return NULL;
+            fast = fast->next;
+        }while(slow!=fast);
+        fast = head;
+        while(slow!=fast){
+            slow=slow->next;
+            fast = fast->next;
+        }
+        return fast;
+    }
+};
